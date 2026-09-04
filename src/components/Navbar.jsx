18f9feb49ee4ac79aa85318a7a0193375a3aba1e
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-09-04 10:30:08 +0200

    Navbar.jsx: Optimer semantisk html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,17 @@ import { NavLink } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="site-nav">
+    <nav className="site-nav" aria-label="Hovednavigation">
       <NavLink className="brand" to="/">
         mellemrum<span>.</span>
       </NavLink>
-      <div className="nav-links">
-        <NavLink to="/">Events</NavLink>
-        <NavLink to="/tilmeldinger">Se tilmeldinger</NavLink>
-        <NavLink to="/om">Om Mellemrum</NavLink>
-      </div> 
+      <div className="nav-links" aria-label="Mellemrum – gå til forsiden">
+        <ul>
+          <li><NavLink to="/">Events</NavLink></li>
+          <li><NavLink to="/tilmeldinger">Se tilmeldinger</NavLink></li>
+          <li><NavLink to="/om">Om Mellemrum</NavLink></li>
+        </ul>
+      </div>
     </nav>
   );
 }
